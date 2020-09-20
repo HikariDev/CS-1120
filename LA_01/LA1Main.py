9ec6526d9f3ba08e702384a98f1a6fb522f6dc35
Andrew Kroll <hikaridev@users.noreply.github.com>
2020-09-20 06:17:22 -0400

    This is stupid, but here we go I guess...
    Completed Assignment I with comments. Still need to BS through the SLC report and attach it shortly...

--- LA_01/LA1Main.py
+++ LA_01/LA1Main.py
@@ -1,8 +1,42 @@
+# Name: Andrew Kroll
+# Date: 2020-09-20
+# Course-Section/LA#: CS1120-951 LA1
+# Description: Generate and manipulate two two-dimensional arrays that are
+#              sized based on validated user input.
+# Note: The function names given in the LA1_F'20.docx file do not comply with
+#       Python naming conventions and have been corrected for this application.
+
 from LA_01 import ProcessList
 
 
 def main():
-    pass
+    rows, cols = 0, 0  # Declare placeholder variables for row and col numbers
+    while not 5 <= rows <= 20:  # Continue asking user for input until between
+        # 5 and 20 (inclusive)
+        try:
+            rows = int(input("Enter number of rows in range [5, 20]: "))
+            if not 5 <= rows <= 20:  # Alert user if outside range
+                print("Invalid input.")
+        except ValueError:  # Alert user if not an integer
+            print("Input must be an integer.")
+    while not 5 <= cols <= 20 or cols == rows:  # Continue asking user for
+        # input until between 5 and 20 (inclusive), not equal to rows
+        try:
+            cols = int(input("Enter number of cols in range [5, 20], "
+                             "except {}: ".format(rows)))
+            if not 5 <= cols <= 20 or rows == cols:  # Alert user if outside
+                # range
+                print("Invalid input.")
+        except ValueError:  # Alert user if not an integer
+            print("Input must be an integer.")
+
+    process_list = ProcessList.ProcessList(rows, cols)  # Generate process_list
+    # object
+    process_list.randomly_fill_list()  # Fill the random numbers array with
+    # random numbers
+    process_list.compute_list_values()  # Compute values for computed values
+    # array
+    process_list.print_list()  # Display both the random and computed arrays
 
 
 main()
---
+++ LA_01/ProcessList.py
@@ -0,0 +1,92 @@
+# Name: Andrew Kroll
+# Date: 2020-09-20
+# Course-Section/LA#: CS1120-951 LA1
+# Description: Handle data processing for array manipulation.
+
+import random
+
+
+class ProcessList:
+
+    def __init__(self, rows: int, cols: int):  # Initializes the ProcessList
+        # object
+        self.rows = rows  # Defines the number of rows
+        self.cols = cols  # Defines the number of columns
+        self.random_numbers = [[0 for col in range(cols)]
+                               for row in range(rows)]  # Define random_numbers
+        # variable as a two dimensional array of 0's with rows rows and cols
+        # cols
+        self.computed_numbers = [[0 for col in range(cols)]
+                                 for row in range(rows)]  # Define
+        # computed_numbers variable as a two dimensional array of 0's with rows
+        # rows and cols cols
+
+    def randomly_fill_list(self):  # Fills the random_numbers array with
+        # randomly generated numbers between 0 and 15
+        for row in range(self.get_rows()):  # Iterate through row values
+            for col in range(self.get_rows()):  # Iterate through col values
+                self.set_random_number(row, col, random.randint(0, 15))
+                # Set the cell to a random number between 0 and 15 (inclusive)
+
+    def compute_list_values(self):  # Fills the computed_numbers array with
+        # the sum of all neighboring cells from random_numbers array
+        for row in range(self.get_rows()):  # Iterate through row values
+            for col in range(self.get_rows()):  # Iterate through col values
+                neighbor_sum = 0  # Used to temporarily hold the sum of all
+                # neighboring cells
+                for n_row in range(max(0, row-1), min(self.get_rows(), row+2)):
+                    # Iterate through neighboring rows
+                    for n_col in range(max(0, col-1), min(self.get_rows(),
+                                                          col+2)):
+                        # Iterate through neighboring cols
+                        if n_row != row or n_col != col:
+                            # Prevent adding current cell to neighbor cell sum
+                            neighbor_sum += self.get_random_number(n_row,
+                                                                   n_col)
+                self.set_computed_number(row, col, neighbor_sum)
+                # Set computed cell value
+
+    def print_list(self):  # Displays both the random_numbers and
+        # computed_numbers arrays with appropriate padding.
+        print("Initial list with random numbers:")
+        for row in range(self.rows):  # Iterate through row values
+            for col in range(self.cols):  # Iterate through col values
+                print(" {:>3n}".format(self.get_random_number(row, col)),
+                      end='')  # Print cell aligned to right with 3 digit
+                # padding
+            print()  # Ends the current row's line
+        print("Computed list:")
+        for row in range(self.rows):  # Iterate through row values
+            for col in range(self.cols):  # Iterate through col values
+                print(" {:>3n}".format(self.get_computed_number(row, col)),
+                      end='')  # Print cell aligned to right with 3 digit
+                # padding
+            print()  # Ends the current row's line
+
+    def get_rows(self) -> int:  # Gets the set number of rows
+        return self.rows
+
+    def set_rows(self, rows: int):  # Updates the number of rows
+        self.rows = rows
+
+    def get_cols(self) -> int:  # Gets the set number of cols
+        return self.cols
+
+    def set_cols(self, cols: int):  # Updates the number of cols
+        self.cols = cols
+
+    def get_random_number(self, row: int, col: int) -> int:
+        # Gets the targeted cell value from random_numbers array
+        return self.random_numbers[row][col]
+
+    def set_random_number(self, row: int, col: int, val: int):
+        # Sets the targeted cell value in random_numbers array
+        self.random_numbers[row][col] = val
+
+    def get_computed_number(self, row: int, col: int) -> int:
+        # Gets the targeted cell value from computed_numbers array
+        return self.computed_numbers[row][col]
+
+    def set_computed_number(self, row: int, col: int, val: int):
+        # Sets the targeted cell value in computed_numbers array
+        self.computed_numbers[row][col] = val
